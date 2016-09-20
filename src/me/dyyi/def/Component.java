3c4e1ad0028d66f2ee47ff90937ce650844d79a6
Dongyoun Yi <dongyoun.yi@gmail.com>
2016-09-20 15:39:32 +0900

    Component 파싱 작업 중

--- src/me/dyyi/def/Component.java
+++ src/me/dyyi/def/Component.java
@@ -1,10 +1,34 @@
 package me.dyyi.def;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 enum Place {
 	FIXED, // Specifies that the component has a location and cannot be moved by automatic tools, but can be moved using interactive commands. You must specify the components' location and orientation.
 	COVER, // Specifies that the component has a location and is a part of a cover macro. A COVER component cannot be moved by automatic tools or interactive commands. You must specify the component's location and its orientation.
 	PLACED, // Specifies that the component has a location, but can be moved using automatic layout tools. You must specify tha component's location and orientation.
-	UNPLACED // Specifies that the component does not have a location
+	UNPLACED; // Specifies that the component does not have a location
+	public Place setPlace(String in) {
+		switch (in) {
+		case "FIXED":
+			return Place.FIXED;
+		case "COVER":
+			return Place.COVER;
+		case "PLACED":
+			return Place.PLACED;
+		case "UNPLACED":
+			return Place.UNPLACED;
+		}
+		return Place.UNPLACED;
+		// Need to throw exception!!
+	}
+	@Override
+	public String toString() {
+		switch(this) {
+		case 
+		}
+		return super.toString();
+	}
 }
 
 enum Soruce {
@@ -64,13 +88,12 @@ public class Component {
 		this.y = null;
 		this.orientation = null;
 	}
-	
 	public Component(String str) {
 		this();
 		this.parse(str);
 	}
 	
-		/* Setter and Getter */
+	/* Setter and Getter */
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
 	public String getModelName() { return modelName; }
@@ -85,7 +108,19 @@ public class Component {
 	public void setOrientation(Orientation orientation) { this.orientation = orientation; }
 
 	public void parse(String str) {
+		Matcher matcher;
+		matcher = Pattern.compile("^\\s*-\\s+(?<name>\\S+)\\s+(?<modelName>\\S+)").matcher(str);
+		if (matcher.find()) {
+			this.name = matcher.group("name");
+			this.modelName = matcher.group("modelName");
+		}
 		
+		matcher = Pattern.compile("\\+\\s+(?<type>FIXED|COVER|PLACED)?\\s*\\(\\s*(?<x>[0-9\\.e\\+\\-]+)\\s+(?<y>[0-9\\.e\\+\\-]+)\\s+\\)\\s+(?<orientation>\\S+)").matcher(str);
+		if (matcher.find()) {
+			this.place = matcher.group("type");
+			this.x = Double.parseDouble(matcher.group("x"));
+			this.y = Double.parseDouble(matcher.group("y"));
+		}
 	}
 	
 	@Override
@@ -107,6 +142,8 @@ public class Component {
 		c.setOrientation(Orientation.FN);
 		System.out.println(c);
 		String test_str = " - core/U1 INVX0_RVT + PLACED ( 15504 3344 ) N ;";
+		c = new Component(test_str);
+		System.out.println(c);
 		// TODO Auto-generated method stub
 	}
 
